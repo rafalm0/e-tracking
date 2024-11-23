0f19c2eb8e4b0d59ec19bd693c91a6c44de08be1
Rafael almeida <rafael.alm@edu.unifor.br>
2024-11-22 23:27:05 -0500

    .

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ app = Flask(__name__)
 # Generate tracking pixel in memory
 pixel_data, mime_type = pytracking.get_open_tracking_pixel()
 
+
 @app.route('/track/<tracking_data>')
 def track(tracking_data):
     # Log that an email was opened
     print(f"Email opened! Tracking data: {tracking_data}")
     return Response(pixel_data, mimetype=mime_type)
+
 
 if __name__ == '__main__':
     app.run()
